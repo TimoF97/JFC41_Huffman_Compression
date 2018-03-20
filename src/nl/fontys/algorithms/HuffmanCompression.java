146c4c57b5917a2b4bc56cf4561b9a5d468dd447
Timo <t.fudala@student.fontys.nl>
2018-03-20 10:18:35 +0100

    #saveBitSetAndTree has been added and implemented.

--- src/nl/fontys/algorithms/HuffmanCompression.java
+++ src/nl/fontys/algorithms/HuffmanCompression.java
@@ -1,7 +1,11 @@
 package nl.fontys.algorithms;
 
+import nl.fontys.utilities.Constants;
 import nl.fontys.utilities.Node;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.BitSet;
 import java.util.HashMap;
 import java.util.PriorityQueue;
@@ -17,6 +21,7 @@ public class HuffmanCompression {
 
     /**
      * This method will encode the given text by the use of the HuffmanCompression.
+     *
      * @param text The text that should be encoded.
      *             This parameter is not allowed to be null.
      */
@@ -25,16 +30,17 @@ public class HuffmanCompression {
 
         nodePriorityQueue = getPrioritizedNodes(text);
         generateBitCodeForNode(generateTree(nodePriorityQueue), "");
-        final BitSet encodedText = getBitsetFromEncodedString(getEncodedString(text));
+        saveBitSetAndTree(filledTreeHashMap, getBitsetFromEncodedString(getEncodedString(text)), Constants.ENCODED_TEXT_FILE_PATH);
     }
 
     /**
      * This method will return a PriorityQueue containing objects of the Node class from the given text.
      * The nodes within the queue will be ordered by the their corresponding frequency in the given text.
+     *
      * @param text The text that the nodes should be calculated for.
      *             This parameter is not allowed to be null.
      * @return Returns an instance of the PriorityQueue class, containing objects of the Node class.
-     *         The nodes will be ordered by their frequency.
+     * The nodes will be ordered by their frequency.
      */
     private PriorityQueue<Node> getPrioritizedNodes(final String text) {
         if (text == null) throw new IllegalArgumentException("The text is not allowed to be null.");
@@ -57,6 +63,7 @@ public class HuffmanCompression {
 
     /**
      * This method generates a decision-tree for the given nodes.
+     *
      * @param nodes The nodes that the decision-tree should be generated for.
      *              This parameter is an instance of the PriorityQueue class, filled with objects of the Node class.
      *              This parameter is not allowed to be null.
@@ -78,19 +85,20 @@ public class HuffmanCompression {
      * This method will generate all the bitcodes for the nodes within the priorityqueue of nodes.
      * The generated bitcodes will be inserted in a HashMap called filledTreeHashMap, which is a representation of all the nodes with their according bitcodes.
      * This HashMap will be the actual 'tree'.
-     *
+     * <p>
      * The 'tree' that will be filled with the characters and their generated bitcodes will be a HashMap.
      * This will be a HashMap because a HashMap is generally the most efficient implementation of the Map class.
      *
-     * @param node The node on which the generation of the bitcode should be applied on.
-     *             This parameter is not allowed to be null.
+     * @param node           The node on which the generation of the bitcode should be applied on.
+     *                       This parameter is not allowed to be null.
      * @param currentBitcode The already generated part of the bitcode for the current node.
      *                       This parameter is not allowed to be null.
      *                       This parameter is indeed allowed to be an empty String as there won't be a currentBitcode when starting the entire generation process.
      */
     private void generateBitCodeForNode(final Node node, final String currentBitcode) {
         if (node == null) throw new IllegalArgumentException("The node is not allowed to be null.");
-        else if (currentBitcode == null) throw new IllegalArgumentException("The currentBitcode is not allowed to be null. If there is no currentBitcode available, use an empty String as parameter.");
+        else if (currentBitcode == null)
+            throw new IllegalArgumentException("The currentBitcode is not allowed to be null. If there is no currentBitcode available, use an empty String as parameter.");
 
         /* The current node has a right node as well. Let's generate the bitcode for it's right node. */
         if (node.getRightNode() != null) {
@@ -121,6 +129,7 @@ public class HuffmanCompression {
 
     /**
      * This method will encode a given String by the use of the generated tree.
+     *
      * @param text The text that should be encoded.
      *             This parameter is not allowed to be null.
      * @return Returns the encoded String. This value is a String.
@@ -138,6 +147,7 @@ public class HuffmanCompression {
 
     /**
      * This method will transform a given encodedString into an object of the BitSet class.
+     *
      * @param encodedString The encodedString that should be transformed into a BitSet.
      *                      This parameter is not allowed to be null.
      * @return Returns an object of the BitSet class containing the bits of the given encodedString.
@@ -158,5 +168,28 @@ public class HuffmanCompression {
             indexCounter++;
         }
         return bitSet;
+    }
+
+    /**
+     * This method will save the generated tree and BitSet to a file for the given filepath.
+     * @param tree The 'tree' that should be saved.
+     *             The 'tree' is an object of the HashMap class using HashMap<Character, String>.
+     *             This parameter is not allowed to be null.
+     * @param bitSet The BitSet that should be saved. The BitSet should be the encoded text.
+     *               This parameter is not allowed to be null.
+     * @param filepath The filepath that the file will have to be saved in.
+     *                 This parameter is not allowed to be null nor an empty String.
+     */
+    private void saveBitSetAndTree(final HashMap<Character, String> tree, final BitSet bitSet, final String filepath) {
+        if (tree == null) throw new IllegalArgumentException("The tree is not allowed to be null.");
+        else if (bitSet == null) throw new IllegalArgumentException("The bitSet is not allowed to be null.");
+        else if (filepath == null || filepath.isEmpty()) throw new IllegalArgumentException("The filepath is not allowed to be null nor an empty String.");
+
+        try (final ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filepath))) {
+            objectOutputStream.writeObject(tree);
+            objectOutputStream.writeObject(bitSet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/nl/fontys/utilities/Constants.java
@@ -0,0 +1,6 @@
+package nl.fontys.utilities;
+
+public class Constants {
+
+    public static final String ENCODED_TEXT_FILE_PATH = "C:\\Users\\Timo\\Dropbox\\Fontys\\Semester 4\\JCF41\\Huffman Compression\\EncodedText.bin";
+}
